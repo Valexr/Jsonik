85693b35707f5b0e13a4fe660d8162667913f6e5
Valexr <valexr@gmail.com>
2023-05-13 13:01:41 +0300

    cleanup

--- src/server/app.ts
+++ src/server/app.ts
@@ -4,17 +4,18 @@ import { files } from '$server/endpoints/files';
 import { logs } from '$server/endpoints/logs';
 import { log } from '$server/middlewares/log';
 import { auth } from '$server/endpoints/auth/index';
-// import { cookie } from '$server/endpoints/auth/cookie';
+import { cookie } from '$server/endpoints/auth/cookie';
 import { token } from '$server/endpoints/auth/token';
 import type { App } from '$server/http/types';
-// import { HttpBadRequest, HttpNotFound, HttpInternalServer, HttpResponseCodes } from '$server/lib/errors';
 
 const DEV = process.env.NODE_ENV === 'dev';
+const PORT = process.env.PORT || 8080
+const CACHE = process.env.CACHE || 0
 
 export const app = server({
     host: '0.0.0.0',
-    port: DEV ? 8080 : 8888,
-    cache: DEV ? 0 : 3600,
+    port: Number(PORT),
+    cache: Number(CACHE),
     compress: !DEV
 })
 
