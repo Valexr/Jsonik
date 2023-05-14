67df91bcf295bc3043827ab100f32a6095e27ee7
Valexr <valexr@gmail.com>
2023-05-14 12:32:53 +0300

    cleanup html env

--- env/html.js
+++ env/html.js
@@ -1,4 +1,4 @@
-import { readFile, writeFile, mkdir } from 'fs/promises';
+import { readFile, writeFile } from 'fs/promises';
 
 const icons = [
     "database",
@@ -44,22 +44,23 @@ export default function html(opts) {
         name: 'html',
         setup(build) {
             build.onEnd(async (result) => {
-                let html = await readFile(options.in, { encoding: 'utf8' });
-                let sprite = await readFile(options.sprite, { encoding: 'utf8' });
+                const html = await readFile(options.in, { encoding: 'utf8' });
+                const sprite = await readFile(options.sprite, { encoding: 'utf8' });
 
                 const used = options.icons.join('"|');
                 const unused = new RegExp(`(<symbol id="(?!${used}")(.|\n)*?symbol>)`, 'g');
-                sprite = sprite.replace(unused, '').replace(/>\s+</g, '><');
+                const icons = sprite.replace(unused, '');
 
                 const [js, css] = result.outputFiles;
 
-                html = html
-                    .replace('</head>', () => `<script type="module">\n${js.text}</script>\n</head>`)
-                    .replace('</head>', () => `<style>\n${css.text}</style>\n</head>`)
-                    .replace('<body>', () => `<body>\n${sprite}\n`)
-                    .replace(/>\s+</g, '><');
+                function replacer(k, v) {
+                    return v.replace('</head>', () => `<script type="module">\n${js.text}</script>\n</head>`)
+                        .replace('</head>', () => `<style>\n${css.text}</style>\n</head>`)
+                        .replace('<body>', () => `<body>\n${icons}\n`)
+                        .replace(/>\s+</g, '><');
+                }
 
-                await writeFile(options.out, `${JSON.stringify(html)}`);
+                await writeFile(options.out, JSON.stringify(html, replacer));
             });
         },
     };
--- src/client/routes/logs/nav.svelte
+++ src/client/routes/logs/nav.svelte
@@ -1,22 +1,16 @@
 <script lang="ts" context="module">
     import { query } from "svelte-pathfinder";
     import { logs } from "$client/stores/logs";
-    import Await from "$client/components/Await.svelte";
     import Paginator from "$client/components/Paginator.svelte";
 </script>
 
 <script lang="ts">
-    query.set({
-        limit: 100,
-        page: 1,
-    });
+    query.set({ limit: 100, page: 1 });
 </script>
 
-<!-- <Await promise={logs.getTotal()} let:result> -->
 <Paginator
     total={$logs.total}
     bind:limit={$query.limit}
     bind:active={$query.page}
     spread={3}
 />
-<!-- </Await> -->
