aae42f261f064aad9fe110aa76c53081697fd96c
Valexr <valexr@gmail.com>
2023-05-17 15:02:46 +0300

    add stop dev client

--- env/reload.js
+++ env/reload.js
@@ -1,6 +1,6 @@
 const es = new EventSource('http://localhost:8000/esbuild');
 
-es.onerror = console.log;
+es.onerror = () => location.reload(true);
 es.onmessage = console.log;
 
 es.addEventListener('change', (e) => {
